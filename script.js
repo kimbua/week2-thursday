d8d5028ca785f435faec2184036b2fee0b634399
Nguyen Lien <nguyen.k.lien98@gmail.com>
2021-07-02 09:50:15 +0700

    step9

--- script.js
+++ script.js
@@ -127,6 +127,13 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+document.addEventListener("mousemove", mouseMoveHandler, false);
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
 function drawScore() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
